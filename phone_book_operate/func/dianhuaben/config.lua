78696c1c0b11879f0b7564d93971c272d44afeb2
kobe.huang <kobe.huang@temaiol.com>
2017-03-02 18:29:33 +0800

    kobe 修改部分文件

---
+++ phone_book_operate/func/dianhuaben/config.lua
@@ -0,0 +1,8 @@
+sl_log_file       = "/private/var/touchelf/scripts/sl/sl_log.txt" --配置文件
+sl_error_time     = 1;  --容错处理
+--end sl_package_config.lua
+is_delete_contact = false; --是否是执行删除操作
+
+--全局配置参数
+pre_fix_phone_numb   = {1865571}--1388888, 1355555, 1344444,1366666}--号段前缀，可以添加多个号码段
+pre_fix_name = {"艾","毕","蔡","代","厄","方","甘","黄","马","赵","钱","孙","李","周","吴","郑","王"};
